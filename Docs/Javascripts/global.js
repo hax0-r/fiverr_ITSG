d4d84cdb85b96400a55396ab728ad498ab24f1ab
Muhammad <talha18513@gmail.com>
2024-11-03 20:59:15 +0500

    color change

--- Docs/Javascripts/global.js
+++ Docs/Javascripts/global.js
@@ -4,3 +4,12 @@ function raf(time) {
     requestAnimationFrame(raf);
 }
 requestAnimationFrame(raf);
+
+setTimeout(() => {
+    const loaderDiv = document.querySelector('.LoaderDiv2');
+    loaderDiv.style.opacity = '0';
+
+    setTimeout(() => {
+        loaderDiv.style.display = 'none';
+    }, 500);
+}, 2000); 
